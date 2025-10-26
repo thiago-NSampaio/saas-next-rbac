b8eb07992fe4e748c0e087a56fd2fe77b34dda8b
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2025-10-25 21:33:08 -0400

    documentation with swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,8 +1,10 @@
 import fastifyCors from '@fastify/cors'
 import {fastify} from 'fastify'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 
 import {
-    // jsonSchemaTransform,
+    jsonSchemaTransform,
     serializerCompiler,
     validatorCompiler,
     type ZodTypeProvider
@@ -17,6 +19,31 @@ app.setValidatorCompiler(validatorCompiler)
 app.register(fastifyCors)
 
 app.register(createAccount)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS with multi-tenant & RBAC.',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI,{
+    routePrefix:'/docs'
+}
+)
 
 app.listen({port:3333}).then(()=>{
     console.log('HTTP server running')
